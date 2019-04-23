707e0a3a48fb4ea464e9cdf508b703e5ec364f5e
Author Name <Author Email>
2019-04-23 11:48:09 +0800

    01,02,03,README add 4.23

--- dj03/index/urls.py
+++ dj03/index/urls.py
@@ -11,3 +11,9 @@ urlpatterns = [
     url(r'^01-parent/$',views.parent),
     url(r'^02-child/$',views.child),
 ]
+urlpatterns +=[
+    url(r'^03-create/$',views.create),
+    url(r'^03-book/$',views.pbk),
+    url(r'^04-retrieve/$',views.retrieve),
+    url(r'05-filter/$',views.filter_show)
+]
--- dj03/index/views.py
+++ dj03/index/views.py
@@ -1,6 +1,9 @@
+import datetime
 from django.shortcuts import render
+from django.http import HttpResponse
 # Create your views here.
-
+#导入模型
+from .models import *
 
 
 
@@ -12,3 +15,72 @@ def parent(request):
 def child(request):
     name = "小白"
     return render(request,'02-child.html',locals())
+
+def create(request):
+    # au1 = Author.objects.create(name='小王',
+    #                       age='30',
+    #                       email='xiaowang@122.com'
+    #                       )
+    # print(au1)
+    # pp = "%s %s %s %s"%(au1.id,au1.name,au1.age,au1.email)
+
+    # au = Author(name="小吴",age="15")
+    # au.email = "xaionw@12.com"
+    # au.save()
+    # pp = "%s %s %s %s" % (au.id, au.name, au.age, au.email)
+
+    dic={
+        "name":"小菜",
+        "age":45,
+        "email":"xiaocau@21.com",
+        "isActive":False
+    }
+    cc = Author(**dic)
+    cc.save()
+    pp = "%s %s %s %s" % (cc.id, cc.name, cc.age, cc.email)
+    return HttpResponse(pp)
+
+def pbk(request):
+    dic = {
+        "title":"电饭锅",
+        "publicate_date":"2021-2-2",
+    }
+    obj = Book(**dic)
+    obj.save()
+    return HttpResponse("book保存成功")
+
+def retrieve(request):
+    # ret = Author.objects
+    # print(ret)
+
+    qq = ""
+    # ret = Author.objects.all()
+    # for i in ret:
+    #     ti = "%s %s %s %s %s"%(i.id,i.name,i.age,i.email,i.isActive)
+    #     qq += "<p>"+ti+"</p>"
+    #     print(ti)
+
+    # at = Author.objects.values()
+    # for i in at:
+    #     ti = "%s %s %s %s %s"%(i['id'],i['name'],i['age'],i['email'],i['isActive'])
+    #     qq += "<p>" + ti + "</p>"
+
+    at = Author.objects.values_list()
+    for i in at:
+        ti = "%s  %s  %s  %s  %s"%(i[0],i[1],i[2],i[3],i[4])
+        qq += "<p>" + ti + "</p>"
+    return HttpResponse(qq)
+
+def filter_show(request):
+    qq = ""
+    # tt = Author.objects.filter(id=1,name='小王')
+    # if tt:
+    #     for aa in tt:
+    #         print(aa.name,aa.email)
+
+    tt = Author.objects.filter()
+    return HttpResponse(pp)
+
+
+
+
